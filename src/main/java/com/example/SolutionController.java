e1fbf4f4d6b924ef36fba3797495a3d6a07cbb47
BjRkaZs <bajuszrekazsofi@ktch.hu>
2025-02-11 13:27:56 +0100

    first commit

---
+++ src/main/java/com/example/SolutionController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SolutionController {
+
+    @FXML
+    private TextField lengthField;
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField resultField;
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+        String radiusStr = this.radiusField.getText();
+        double radius = Double.parseDouble(radiusStr);
+        String lengthStr = this.lengthField.getText();
+        double length = Double.parseDouble(lengthStr);
+        Double surface = this.calcSurface(radius, length);
+        this.resultField.setText(surface.toString());
+    }
+
+    private Double calcSurface(double radius, double length){
+        return 2 * Math.PI * radius * (radius + length);
+    }
+
+}
